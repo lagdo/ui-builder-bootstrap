51943ab5682ec7f517085b27dc58a82a98f1b418
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2022-02-04 04:15:20 +0100

    Added Bootstrap 5 support.

--- src/start.php
+++ src/start.php
@@ -7,4 +7,6 @@ if (function_exists('jaxon')) {
     $di->alias('dbadmin_builder_bootstrap3', Lagdo\UiBuilder\Bootstrap\Bootstrap3\Builder::class);
     $di->auto(Lagdo\UiBuilder\Bootstrap\Bootstrap4\Builder::class);
     $di->alias('dbadmin_builder_bootstrap4', Lagdo\UiBuilder\Bootstrap\Bootstrap4\Builder::class);
+    $di->auto(Lagdo\UiBuilder\Bootstrap\Bootstrap5\Builder::class);
+    $di->alias('dbadmin_builder_bootstrap5', Lagdo\UiBuilder\Bootstrap\Bootstrap5\Builder::class);
 }
